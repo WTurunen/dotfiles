26c7f256c0fb21e7e2e440f85dc1d5549ef42ac9
Waltteri Turunen <waltteri@protonmail.com>
2017-03-02 15:33:46 +0200

    Update alias.zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,9 +2,6 @@
 # Z Shell aliases
 #
 
-alias ll='ls -lFh'
-alias psl='ps -e -o user,pid,ppid,stat,start_time,%cpu,cputime,sz,rss,%mem,nlwp,args -H'
-
 # Color support
 if [ -x /usr/bin/dircolors ]; then
     alias ls='ls --color=auto'
@@ -13,6 +10,10 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep='egrep --color=auto'
 fi
 
+alias la='ls -lFha'
+alias ll='ls -lFh'
+alias phpgrep='grep -rin --include="*.php"'
+alias psl='ps -e -o user,pid,ppid,stat,start_time,%cpu,cputime,sz,rss,%mem,nlwp,args -H'
+
 # execute last typed command with sudo
 alias please='sudo zsh -c "$(fc -ln -1)"'
-
